2cf49574df5ea04bc7901381de18019ecf4071a0
hikari97 <h.makki97@gmail.com>
2025-12-19 16:48:44 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { AwsClient } from 'aws4fetch';
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request, env) {
+		const url = new URL(request.url);
+		const objectKey = url.pathname.replace(/^\/+/, '');
+
+		if (!objectKey) {
+			return new Response('Not Found', { status: 404 });
+		}
+
+		const aws = new AwsClient({
+			accessKeyId: env.IDRIVE_ACCESS_KEY,
+			secretAccessKey: env.IDRIVE_SECRET_KEY,
+			region: env.IDRIVE_REGION,
+			service: 's3',
+		});
+
+		const s3Url = `https://${env.IDRIVE_BUCKET}.${env.IDRIVE_REGION}.idrivee2.com/${objectKey}`;
+
+		const s3Response = await aws.fetch(s3Url);
+
+		return new Response(s3Response.body, {
+			headers: {
+				'Content-Type': s3Response.headers.get('Content-Type') ?? 'application/octet-stream',
+				'Cache-Control': 'public, max-age=31536000',
+			},
+		});
 	},
 };
